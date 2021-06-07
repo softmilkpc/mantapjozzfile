fb7059d477b00c246743a5d1af5e00d998726d09
softmilkpc <70849324+softmilkpc@users.noreply.github.com>
2021-06-07 18:43:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-#(©)Codexbotz
-
 import pyromod.listen
 from pyrogram import Client
 import sys
@@ -31,7 +29,7 @@ class Bot(Client):
             except:
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning("Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/mantapfilestorage ")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -41,11 +39,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning("Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/mantapfilestorage")
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nJoin Channel:\n @mantapvids\n@mantapfilestorage")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
